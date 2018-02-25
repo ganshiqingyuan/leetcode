caf45d8e6cd6f7add2f4abc9f6a94dea0048140b
liu <1174607294@qq.com>
2018-02-25 14:38:18 +0800

    hellohello

---
+++ string/Find Smallest Letter Greater Than Target.js
@@ -0,0 +1,64 @@
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+//2018 2 25
+//js bird
+
+Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+
+Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+Examples:
+Input:
+letters = ["c", "f", "j"]
+target = "a"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "c"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "d"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "g"
+Output: "j"
+
+Input:
+letters = ["c", "f", "j"]
+target = "j"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "k"
+Output: "c"
+Note:
+letters has a length in range [2, 10000].
+letters consists of lowercase letters, and contains at least 2 unique letters.
+target is a lowercase letter.
+
+
+
+
+
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    var result='z',falg=false
+    for(var i=0;i<letters.length;i++){
+        if(letters[i]>target&&result>=letters[i]){
+            falg=true
+            result=letters[i]
+        }
+    }
+    return !falg?letters[0]:result
+};
+//、水题
